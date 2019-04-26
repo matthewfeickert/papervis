14884b1c469addcc86b8b82f1683dd4fd8b298a4
Matthew Feickert <matthew.feickert@cern.ch>
2019-04-26 14:51:16 -0500

    Add conversion and animation function

--- README.md
+++ README.md
@@ -16,5 +16,5 @@ git clone depth -1 https://github.com/matthewfeickert/papervis.git /opt/papervis
 
 1. Get the HTTPS URL of the Git repo
 2. Get the commit hash of the Git repo to start at
-3. Run the driver to generate the files for each commit (`bash driver.sh $git_repo_HTTPS $start_commit_hash`)
-4. Convert PDFs to PNGs and then animate to `.mp4` with `ffmpeg` (`bash make_all_nup.sh`)
+3. Run the driver (`bash driver.sh $git_repo_HTTPS $start_commit_hash 9x6`)
+4. Check in the `build` directory for the output
--- scripts/driver.sh
+++ scripts/driver.sh
@@ -38,6 +38,41 @@ function make_all() {
     done < <(git rev-list --reverse "${1}..master")
 }
 
+function make_all_nup() {
+    # 1: the dimensionality of the paper grid
+    local grid_dimension
+    grid_dimension="${1}"
+
+    shopt -s extglob
+    regex="paper-+([0-9]).pdf"
+    max_page_number_size=$(find ${regex} | cut -d- -f2 | cut -d. -f1 | awk '{ print length }' | sort | tail -1)
+
+    # Convert to "n-up" PDF files and rename with zero padded names for order
+    for pdfile in ${regex}; do
+        pdfnup --nup "${grid_dimension}" --suffix "${grid_dimension}" --batch "${pdfile}"
+
+        page_number=$(echo "${pdfile}" | cut -d- -f2 | cut -d. -f1);
+        page_number_size=$(echo "${page_number}" | awk '{ print length }')
+        zero_pad_len=$((max_page_number_size-page_number_size))
+        if (( zero_pad_len > 0 )); then
+            mv "paper-${page_number}-${grid_dimension}.pdf" "$(printf "paper-%0${zero_pad_len}d${page_number}-${grid_dimension}.pdf")"
+        fi
+    done
+
+    # Convert to PNG and animate
+    for pdfile in paper-[0-9]*-$grid_dimension.pdf ; do
+        if [ ! -f "${pdfile%.*}".png ]; then
+            convert -verbose -density 300 -geometry 'x800' -background "#FFFFFF" -flatten "${pdfile}" "${pdfile%.*}".png
+        fi
+    done
+
+    ffmpeg -y -pattern_type glob -i '*.png' -c:v libx264  -vf "fps=24,format=yuv420p" paper-anim.mp4
+
+    for pdfile in paper-[0-9]*-$grid_dimension.pdf ; do
+        echo "${pdfile}"
+    done
+}
+
 function main() {
 
     if [[ $# -gt 0 ]]; then
@@ -53,10 +88,19 @@ function main() {
             sleep 1
 
             make_all "${start_commit_hash}"
+
+            local grid_dimension
+            if [[ $# -gt 2 ]]; then
+                grid_dimension="${3}"
+            else
+                grid_dimension="9x6"
+            fi
+            cd build
+            make_all_nup "${grid_dimension}"
         fi
     fi
 }
 
-# bash driver.sh https://github.com/matthewfeickert/Dedman-Thesis-Latex-Template 1e3a42f6a1bd39275c59ac1e3b713cd7a2d9a927
+# bash scripts/driver.sh https://github.com/matthewfeickert/Dedman-Thesis-Latex-Template 2d8d5ca13127584578cdb9806fef98dbaab60a16
 
 main "$@" || return 1
